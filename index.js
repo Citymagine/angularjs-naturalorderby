7154f321acdd8ccc37890e38f543220edbdc9a8c
Cyril CHAPON <cyril@citymagine.com>
2017-09-07 16:38:36 +0200

    Add dep override main and safe

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('angularjsNaturalorderby', [])
-  .filter('naturalOrderBy', function ($filter) {
+  .filter('naturalOrderBy', function ($filter, $window) {
     return function (array, sortPredicate, reverseOrder) {
       return $filter('orderBy')(array, sortPredicate, reverseOrder, function(a, b) {
-        return naturalSort(a.value, b.value);
+        return $window.naturalSort(a.value, b.value);
       });
     };
   });
